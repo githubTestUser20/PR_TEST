aa9a8f9eb529b03435c348c2931462802884d9f1
githubTestUser21 <info@jotadeveloper.com>
2017-07-07 13:55:39 +0200

    CP-4 Fix code review issues

--- component/input/index.js
+++ component/input/index.js
@@ -1,9 +1,9 @@
 function shortName() {
-    return (IDoSomethingSuperFancyHere(1, 4)) * 5 * 5
+    return (IDoSomethingSuperFancyHere(1, 4)) * 5 * 5;
 }
 
 function IDoSomethingSuperFancyHere(x, y) {
-    return (y + x ) * 5
+    return (y + x ) * 5;
 }
 
 module.exports = fancyComponent1;
